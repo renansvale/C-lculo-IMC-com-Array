d25812b060f0b9908e72dd1f5a50c65afb4259b7
renansvale <64032043+renansvale@users.noreply.github.com>
2021-04-29 17:10:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace array
+{
+    class Program{
+    
+        static void Main(string[] args)
+        {
+            String situacao ="";
+            int q;
+            char continua = 's';
+            Double peso, altura;
+            int quantidade;
+
+            string nome;
+            double imc;
+            Console.WriteLine("Digite a quantidade de pacientes:");
+            quantidade = Convert.ToInt16(Console.ReadLine());
+
+            while (continua == 's');
+                q = 0;
+            do
+            {
+                Console.WriteLine("Digite o nome do paciente:");
+                nome.Add(Console.ReadLine());
+                Console.WriteLine("Digite a altura do paciente:");
+                altura = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o peso do paciente:");
+                peso = Convert.ToDouble(Console.ReadLine());
+                imc.Add(peso / (altura*altura));
+                continua = Convert.ToChar(Console.ReadLine());
+            }
+            while (continua == 's');
+            q = 0;
+
+            for (int y = 0; y < quantidade; y++)
+            {
+                if (imc <=20)
+                {
+                    situacao = "Abaixo do peso";
+                }
+                else
+                {
+                    if (imc >20 && imc <25)
+                    {
+                    situacao = "Peso normal";
+                    }
+                    else
+                    {
+                        if (imc >=25)
+                        {
+                    situacao = "Acima do peso";
+                        }
+                }
+            Console.WriteLine(nome[q] + " o seu IMC é " + imc + " e a sua situação é " + situacao); 
+            q++;  
+            }
+        }
+    }
+}
+}
